8c0ae597bd10c91ae046bc69b86c7cee63f78e59
Infinempty <48058432+Infinempty@users.noreply.github.com>
2022-11-10 23:47:03 +0800

    Create constant_val.h

---
+++ constant_val.h
@@ -0,0 +1,24 @@
+
+
+const int BPLUE_LEVEL=4; 
+//B+树阶数
+const int LENGTH_OF_RECORD=100;
+//记录属性数量
+const int BYTES_OF_RECORD=800;
+//记录字节大小
+const int SHOW_MAX_ROW=20;
+//最大展示行
+const int BYTES_OF_INT=4;
+//int字节数量
+const int BYTES_OF_BOOL=1;
+//bool字节数量
+const int BYTES_OF_INT64=8;
+//属性字节数量
+const int BYTES_OF_TREENODE=101;
+//树节点字节数量
+const char TABLE_NAME[]="./bin/table.txt";
+//表格地址
+const char INDEX_NAME[]="./index/i";
+//索引地址
+const char INDEX_EXISTS_NAME[]="./bin/indexe.txt";
+//索引标记地址
